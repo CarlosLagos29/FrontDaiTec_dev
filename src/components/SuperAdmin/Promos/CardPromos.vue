ea40d0df6fdc914f1a5b2ed3124ce93c5172240b
CarlosLagos29 <137452483+CarlosLagos29@users.noreply.github.com>
2024-08-01 16:23:41 -0300

    se agrego un botton para eliminar elementos de manera reutilizable

--- src/components/SuperAdmin/Promos/CardPromos.vue
+++ src/components/SuperAdmin/Promos/CardPromos.vue
@@ -1,18 +1,22 @@
 <script setup>
+import DeleteButton from '../Reutilizables/Buttons/DeleteButton.vue';
 
 const props = defineProps({
-    promo: Object,
-    order: Number,
+  promo: Object,
+  order: Number,
 })
 </script>
 
 <template>
-    <span :class="{'animate-fadeButton': order % 2 === 0, 'animate-fadeTop': order % 2 !== 0}">
+  <span :class="{ 'animate-fadeButton': order % 2 === 0, 'animate-fadeTop': order % 2 !== 0 }">
     <div class="flex flex-col items-center shadow-custom rounded-md p-4 hover:scale-105 transition-transform">
-        <h1 class="text-center text-2xl font-bold text-titulos my-2">{{ promo.name }}</h1>
-        <a-image class="max-h-96 rounded-md " :src="promo.url" :alt="promo.name"/>
+      <section class="w-full flex justify-end items-center">
+        <DeleteButton :id="promo._id" :type="'promos'" :name="promo.name" />
+      </section>
+      <h1 class="text-center text-2xl font-bold text-titulos my-2">{{ promo.name }}</h1>
+      <a-image class="max-h-96 rounded-md " :src="promo.url" :alt="promo.name" />
     </div>
-</span>
+  </span>
 </template>
 
 <style scoped>
--- src/components/SuperAdmin/Reutilizables/Buttons/DeleteButton.vue
+++ src/components/SuperAdmin/Reutilizables/Buttons/DeleteButton.vue
@@ -1,9 +1,36 @@
 <script setup>
 import { BASE_URL } from "@/GlobalState/store";
+import { message } from "ant-design-vue";
+import { h } from "vue";
+import { DeleteOutlined } from '@ant-design/icons-vue'
+import axios from "axios";
+
+const { id, type, name } = defineProps({
+    id: Number,
+    type: String,
+    name: String,
+});
+
+const handleDelete = async () => {
+    try {
+        await axios.delete(`${BASE_URL}${type}/${id}`);
+
+        const succesMessage = message.success(`Se elimino ${name}`)
+    } catch (error) {
+        const errorMessage = message.error(`Se produjo un error al eliminar ${name}`,0);
+        errorMessage();
+        console.error(error);
+    } finally{
+        const loadingMessage = message.loading(`Eliminando ${name}...`, 0);
+        loadingMessage();
+    }
+};
 
 </script>
 
 <template>
-    
-    
+    <a-popconfirm title="¿Seguro que quieres eliminarlo?" ok-text="Si" cancel-text="No" @confirm="handleDelete">
+        <a-button class=" full-width-button flex justify-center items-center" type="primary" danger
+            :icon="h(DeleteOutlined)" />
+    </a-popconfirm>
 </template>

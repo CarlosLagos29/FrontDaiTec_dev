231b80e7e946187d4d98d1a8c20534e0c9f92b52
CarlosLagos29 <137452483+CarlosLagos29@users.noreply.github.com>
2024-08-15 13:29:55 -0300

    mejora en el forulario de anteriormente createProfecionals, ahora simplemente como formProfecionals para hacerlo reutilizable y q tambien sirva como formulario para editar el profecional

--- src/components/SuperAdmin/ProfecionalsDb/ProfecionalsDb.vue
+++ src/components/SuperAdmin/ProfecionalsDb/ProfecionalsDb.vue
@@ -2,16 +2,19 @@
 import { ref } from 'vue';
 
 import viewProfecional from './viewProfecional.vue';
-import createProfecional from './createProfecional.vue';
+import formProfecional from './formProfecional.vue';
 
 const addProfecional = ref(false);
+
+const createProfecional = formProfecional;
+const editProfecional = formProfecional;
 
 </script>
 
 <template>
     <div>
-        <button  @click="addProfecional = !addProfecional">
-                <span v-if="!addProfecional" class=" border rounded-md px-4 py-2 text-buttons"> + Añadir promo</span>
+        <button class=" my-3 "  @click="addProfecional = !addProfecional">
+                <span v-if="!addProfecional" class=" border rounded-md px-4 py-2"> + Añadir profecional</span>
         </button>
         <viewProfecional v-if="!addProfecional"/>
         <createProfecional v-else @handleCreate="addProfecional = !addProfecional"/>
--- src/components/SuperAdmin/ProfecionalsDb/formProfecional.vue
+++ src/components/SuperAdmin/ProfecionalsDb/formProfecional.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, h, computed } from 'vue';
+import { ref, onMounted } from 'vue';
 import { BASE_URL } from '@/GlobalState/store';
 import { message } from 'ant-design-vue';
 import { useStore } from 'vuex';
@@ -9,12 +9,20 @@ import axios from 'axios';
 
 const { dispatch } = useStore();
 
-const newProfecional = ref({
-    name: "",
-    img: "",
-    description: "",
-    services: [],
-});
+const props = defineProps({
+    profecional: {
+        type: Object,
+        default:() => ({
+            name: "",
+            img: "",
+            description: "",
+            services: [],
+    })
+}
+})
+
+const newProfecional = ref({});
+
 
 const emit = defineEmits(['handleCreate']);
 
@@ -25,19 +33,19 @@ const handleCreateImg = (img) => {
 const currentService = ref("");
 
 const handleSetService = () => {
-   if (currentService.value && !newProfecional.value.services.includes(currentService.value)){
+    if (currentService.value && !newProfecional.value.services.includes(currentService.value)) {
         newProfecional.value.services.push(currentService.value);
 
-    currentService.value = "";
+        currentService.value = "";
 
     }
 };
 
-const handleDeleteService = (service) =>{
+const handleDeleteService = (service) => {
     const index = newProfecional.value.services.indexOf(service);
-  if (index > -1) {
-    newProfecional.value.services.splice(index, 1);
-  }
+    if (index > -1) {
+        newProfecional.value.services.splice(index, 1);
+    }
 }
 
 const cancel = async () => {
@@ -53,23 +61,28 @@ const cancel = async () => {
 };
 
 const submit = async () => {
-    try {
-        await axios.post(`${BASE_URL}users/profecionals/`, newProfecional.value);
-        message.success(`El profecinal "${newProfecional.value.name}" fue creado con exito`);
-        emit('handleCreate');
-        dispatch('getProfecionals');
-    } catch (error) {
-        message.error('Error al cargar el Profecional');
-        console.error(error)
-    }finally {
-            const loadingMessage = message.loading('Subiendo profecional...', 0);
-            loadingMessage();
-        }
+    // try {
+    //     await axios.post(`${BASE_URL}users/profecionals/`, newProfecional.value);
+    //     message.success(`El profecinal "${newProfecional.value.name}" fue creado con exito`);
+    //     emit('handleCreate');
+    //     dispatch('getProfecionals');
+    // } catch (error) {
+    //     message.error('Error al cargar el Profecional');
+    //     console.error(error)
+    // } finally {
+    //     const loadingMessage = message.loading('Subiendo profecional...', 0);
+    //     loadingMessage();
+    // }
+    console.log(newProfecional.value);
 };
+
+onMounted(()=>{
+    newProfecional.value = props.profecional;
+});
 </script>
 
 <template>
-    <form @keydown.enter.prevent @submit.prevent="submit"> 
+    <form @keydown.enter.prevent @submit.prevent="submit">
         <section>
             <button type="button"
                 class=" border border-red-500 rounded-md px-4 py-2 text-buttons hover:text-white hover:bg-red-500 "
@@ -86,16 +99,16 @@ const submit = async () => {
 
         <label> Descripcion: </label>
         <textarea name="description" class="border border-slate-400 px-2 py-1 rounded-md w-full" type="text"
-        v-model="newProfecional.description" cols="200" rows="5" @keydown.enter.prevent="false"></textarea>
+            v-model="newProfecional.description" cols="200" rows="5" @keydown.enter.prevent="false"></textarea>
 
         <label> Servicios:</label>
         <section class=" flex gap-2">
-            <input class="border border-slate-400 px-2 py-1 rounded-md placeholder:italic" placeholder="ejemplo:Rodrigo..."
-            type="text" name="service" v-model="currentService">
+            <input class="border border-slate-400 px-2 py-1 rounded-md placeholder:italic"
+                placeholder="ejemplo:Rodrigo..." type="text" name="service" v-model="currentService">
             <button @click="handleSetService" type="button">Agregar servicio</button>
         </section>
 
-        <ul v-if="newProfecional.services.length" v-for="(s, i) in newProfecional.services">
+        <ul v-if="newProfecional.services?.length" v-for="(s, i) in newProfecional.services">
             <li :key="i">
                 <button @click="handleDeleteService(s)" type="button">{{ s }}</button>
             </li>
